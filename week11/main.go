acfa5ddf8d63bc8fd0bd6ddbbfdba77473a1d312
seokhoon100 <100seokoon@gmail.com>
2024-11-15 10:42:04 +0900

    v3.6 Array

--- week11/main.go
+++ week11/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "week/greeting"
+import (
+	"fmt"
+	"week/greeting"
+)
 
 func main() {
 	greeting.Hi("Inha")
 	greeting.Hello("Harvard")
+}
+
+func Hi() {
+	fmt.Printf("Hi!\n")
+}
+
+func Hello() {
+	fmt.Printf("H!\n")
 }
